2b8eb2b06bf0291a118201ec65a61fb35eba680a
Konstantin <kakoser91@gmail.com>
2024-12-05 15:41:49 +0300

    Add files via upload

---
+++ tests_12_2.py
@@ -0,0 +1,89 @@
+import unittest
+
+
+class Runner:
+    def __init__(self, name, speed=5):
+        self.name = name
+        self.distance = 0
+        self.speed = speed
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+class TournamentTest(unittest.TestCase):
+
+    all_results = {}
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+
+    def setUp(self):
+        self.usain = Runner("Усэйн", 10)
+        self.andrei = Runner("Андрей", 9)
+        self.nick = Runner("Ник", 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for _, result in cls.all_results.items():
+            print({pos: runner.name for pos, runner in result.items()})
+
+
+    def test_race_usain_nick(self):
+        tournament = Tournament(90, self.usain, self.nick)
+        results = tournament.start()
+        self.all_results["Первый забег"] = results
+        last_runner = results[max(results.keys())]
+        self.assertTrue(last_runner == "Ник")
+
+
+    def test_race_andrei_nick(self):
+        tournament = Tournament(90, self.andrei, self.nick)
+        results = tournament.start()
+        self.all_results["Второй забег"] = results
+        last_runner = results[max(results.keys())]
+        self.assertTrue(last_runner == "Ник")
+
+
+    def test_race_usain_andrei_nick(self):
+        tournament = Tournament(90, self.usain, self.andrei, self.nick)
+        results = tournament.start()
+        self.all_results["Третий забег"] = results
+        last_runner = results[max(results.keys())]
+        self.assertTrue(last_runner == "Ник")
+
+
+if __name__ == "__main__":
+    unittest.main()
