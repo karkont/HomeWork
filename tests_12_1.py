0bdccf0c54e25f90509abcad768fc945edffc580
Konstantin <kakoser91@gmail.com>
2024-12-20 12:09:20 +0300

    Add files via upload

--- tests_12_1.py
+++ tests_12_1.py
@@ -5,6 +5,7 @@ class Runner:
         self.name = name
         self.distance = 0
 
+
     def run(self):
         self.distance += 10
 
@@ -15,18 +16,23 @@ class Runner:
         return self.name
 
 class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(False, "")
     def test_walk(self):
         runner = Runner("Test Runner Walk")
         for _ in range(10):
             runner.walk()
         self.assertEqual(runner.distance, 50)
 
+    @unittest.skipIf(False, "")
     def test_run(self):
         runner = Runner("Test Runner Run")
         for _ in range(10):
             runner.run()
         self.assertEqual(runner.distance, 100)
 
+    @unittest.skipIf(False, "")
     def test_challenge(self):
         runner1 = Runner("Runner 1")
         runner2 = Runner("Runner 2")
